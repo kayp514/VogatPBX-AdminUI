32729cd8d2dacf1ca830151aa1a215d94c888a4a
kayp514 <pa.ka12@outlook.com>
2025-04-07 17:46:48 -0400

    Refactor GET function in permissions route to simplify response structure by removing unnecessary fields from the permissions object, enhancing performance and clarity.

--- src/app/api/v1/permissions/route.ts
+++ src/app/api/v1/permissions/route.ts
@@ -7,26 +7,14 @@ export async function GET() {
       select: {
         id: true,
         name: true,
-        content_type_id: true,
         codename: true,
-        django_content_type: {
-          select: {
-            id: true,
-            app_label: true,
-            model: true,
-            main_label: true,
-          },
-        },
       },
     });
 
     const formattedPermissions = permissions.map(permission => ({
       id: permission.id,
       name: permission.name,
-      content_type_id: permission.content_type_id,
       codename: permission.codename,
-      model: permission.django_content_type.model,
-      main_label: permission.django_content_type.main_label,
     }));
 
     return NextResponse.json(formattedPermissions);
